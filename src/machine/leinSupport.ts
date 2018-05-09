be038918388619e2435eaca5fbde7d0c40922363
Christian Dupuis <cd@atomist.com>
2018-05-08 17:31:54 -0700

    add lein-support in

---
+++ src/machine/leinSupport.ts
@@ -0,0 +1,73 @@
+import { Configuration } from "@atomist/automation-client";
+import { GitProject } from "@atomist/automation-client/project/git/GitProject";
+import * as clj from "@atomist/clj-editors";
+import {
+    branchFromCommit,
+    DefaultDockerImageNameCreator,
+    DockerBuildGoal,
+    DockerOptions,
+    executeDockerBuild,
+    ExecuteGoalResult,
+    executeVersioner,
+    IsLein,
+    leinBuilder,
+    ProjectVersioner,
+    RunWithLogContext,
+    SoftwareDeliveryMachine,
+    VersionGoal,
+} from "@atomist/sdm";
+import * as build from "@atomist/sdm/blueprint/dsl/buildDsl";
+import { spawnAndWatch } from "@atomist/sdm/util/misc/spawned";
+import * as df from "dateformat";
+
+export function addLeinSupport(sdm: SoftwareDeliveryMachine,
+                               configuration: Configuration) {
+
+    // TODO cd atomist.sh builder
+    sdm.addBuildRules(
+        build.when(IsLein)
+            .itMeans("Lein build")
+            .set(leinBuilder(sdm.opts.projectLoader, "lein do clean, dynamodb-local test")),
+    );
+
+    sdm.addGoalImplementation("leinVersioner", VersionGoal,
+            executeVersioner(sdm.opts.projectLoader, LeinProjectVersioner), { pushTest: IsLein })
+        .addGoalImplementation("leinDockerBuild", DockerBuildGoal,
+            executeDockerBuild(
+                sdm.opts.projectLoader,
+                DefaultDockerImageNameCreator,
+                [MetajarPreparation],
+                {
+                    ...configuration.sdm.docker.private as DockerOptions,
+                    dockerfileFinder: async () => "docker/Dockerfile",
+                }))
+
+}
+
+export async function MetajarPreparation(p: GitProject, rwlc: RunWithLogContext): Promise<ExecuteGoalResult> {
+    const result = await spawnAndWatch({
+            command: "lein",
+            args: ["with-profile", "metajar", "do", "clean,", "metajar"],
+        },
+        {
+            cwd: p.baseDir,
+        },
+        rwlc.progressLog,
+        {
+            errorFinder: code => code !== 0,
+        });
+    return result;
+}
+
+export const LeinProjectVersioner: ProjectVersioner = async (status, p, log) => {
+    const file = await p.findFile("./project.clj");
+    const projectVersion = clj.getVersion(file.path);
+    const branch = branchFromCommit(status.commit);
+    const branchSuffix = branch !== status.commit.repo.defaultBranch ? `${branch}.` : "";
+    const version = `${projectVersion}-${branchSuffix}${df(new Date(), "yyyymmddHHMMss")}`;
+
+    clj.setVersion(file.path, version);
+
+    return version;
+};
+
--- src/machine/machine.ts
+++ src/machine/machine.ts
@@ -14,85 +14,52 @@
  * limitations under the License.
  */
 
+import { Configuration } from "@atomist/automation-client";
 import {
-    Configuration,
-    logger,
-} from "@atomist/automation-client";
-import { GitProject } from "@atomist/automation-client/project/git/GitProject";
-import {
-    hasFile,
     IsAtomistAutomationClient,
-    nodeBuilder,
+    IsLein,
     not,
-    ProductionEnvironment,
     SoftwareDeliveryMachine,
     SoftwareDeliveryMachineOptions,
-    StagingEnvironment,
     ToDefaultBranch,
     whenPushSatisfies,
 } from "@atomist/sdm";
-import * as build from "@atomist/sdm/blueprint/dsl/buildDsl";
-import { RepoContext } from "@atomist/sdm/common/context/SdmContext";
-import { executeTag } from "@atomist/sdm/common/delivery/build/executeTag";
-import {
-    executePublish,
-    NpmOptions,
-} from "@atomist/sdm/common/delivery/build/local/npm/executePublish";
-import { NodeProjectIdentifier } from "@atomist/sdm/common/delivery/build/local/npm/nodeProjectIdentifier";
-import { NodeProjectVersioner } from "@atomist/sdm/common/delivery/build/local/npm/nodeProjectVersioner";
-import { NpmPreparations } from "@atomist/sdm/common/delivery/build/local/npm/npmBuilder";
-import { executeVersioner } from "@atomist/sdm/common/delivery/build/local/projectVersioner";
-import { tslintFix } from "@atomist/sdm/common/delivery/code/autofix/node/tslint";
-import { PackageLockFingerprinter } from "@atomist/sdm/common/delivery/code/fingerprint/node/PackageLockFingerprinter";
-import {
-    DefaultDockerImageNameCreator,
-    DockerOptions,
-    executeDockerBuild,
-} from "@atomist/sdm/common/delivery/docker/executeDockerBuild";
-import {
-    DockerBuildGoal,
-    NoGoals,
-    TagGoal,
-    VersionGoal,
-} from "@atomist/sdm/common/delivery/goals/common/commonGoals";
+import { NoGoals } from "@atomist/sdm/common/delivery/goals/common/commonGoals";
 import { HasTravisFile } from "@atomist/sdm/common/listener/support/pushtest/ci/ciPushTests";
 import { IsDeployEnabled } from "@atomist/sdm/common/listener/support/pushtest/deployPushTests";
 import { HasDockerfile } from "@atomist/sdm/common/listener/support/pushtest/docker/dockerPushTests";
 import { IsNode } from "@atomist/sdm/common/listener/support/pushtest/node/nodePushTests";
-import { tagRepo } from "@atomist/sdm/common/listener/support/tagRepo";
 import {
     disableDeploy,
     enableDeploy,
 } from "@atomist/sdm/handlers/commands/SetDeployEnablement";
-import { createKubernetesData } from "@atomist/sdm/handlers/events/delivery/goals/k8s/launchGoalK8";
-import { SdmGoal } from "@atomist/sdm/ingesters/sdmGoalIngester";
-import { MaterialChange } from "../support/materialChange";
+import { MaterialChangeToClojureRepo } from "../support/materialChangeToClojureRepo";
+import { MaterialChangeToNodeRepo } from "../support/materialChangeToNodeRepo";
 import { simplifiedDeployment } from "../support/simplifiedDeployment";
-import { AutomationClientTagger } from "../support/tagger";
 import {
     BuildGoals,
     CheckGoals,
     DockerGoals,
     KubernetesDeployGoals,
-    ProductionDeploymentGoal,
-    PublishGoal,
+    LeinDockerGoals,
     SimplifiedKubernetesDeployGoals,
-    StagingDeploymentGoal,
 } from "./goals";
-
-export type MachineOptions = SoftwareDeliveryMachineOptions;
+import { addLeinSupport } from "./leinSupport";
+import { addNodeSupport } from "./nodeSupport";
 
 export function machine(options: SoftwareDeliveryMachineOptions,
                         configuration: Configuration): SoftwareDeliveryMachine {
     const sdm = new SoftwareDeliveryMachine(
-        "Automation Client Software Delivery Machine",
+        "Atomist Software Delivery Machine",
         options,
 
-        whenPushSatisfies(not(MaterialChange))
+        // Node
+
+        whenPushSatisfies(IsNode, not(MaterialChangeToNodeRepo))
             .itMeans("No Material Change")
             .setGoals(NoGoals),
 
-        whenPushSatisfies(HasTravisFile, IsNode)
+        whenPushSatisfies(IsNode, HasTravisFile)
             .itMeans("Just Checking")
             .setGoals(CheckGoals),
 
@@ -118,108 +85,24 @@ export function machine(options: SoftwareDeliveryMachineOptions,
         whenPushSatisfies(IsNode, not(HasDockerfile), not(IsAtomistAutomationClient))
             .itMeans("Module Build")
             .setGoals(BuildGoals),
+
+        // Clojure
+
+        whenPushSatisfies(IsLein, not(MaterialChangeToClojureRepo))
+            .itMeans("No material change")
+            .setGoals(NoGoals),
+
+        whenPushSatisfies(IsLein, not(HasTravisFile), ToDefaultBranch, MaterialChangeToClojureRepo)
+            .itMeans("Build a Clojure Service with Leinigen")
+            .setGoals(LeinDockerGoals),
+
     );
 
-    sdm.addSupportingCommands(enableDeploy, disableDeploy)
-        .addNewRepoWithCodeActions(tagRepo(AutomationClientTagger))
-        .addAutofixes(tslintFix)
-        .addFingerprinterRegistrations(new PackageLockFingerprinter());
+    sdm.addSupportingCommands(enableDeploy, disableDeploy);
 
-    const hasPackageLock = hasFile("package-lock.json");
-
-    sdm.addBuildRules(
-        build.when(hasPackageLock)
-            .itMeans("npm run build")
-            .set(nodeBuilder(options.projectLoader, "npm ci", "npm run build")),
-        build.when(not(hasPackageLock))
-            .itMeans("npm run build (no package-lock.json)")
-            .set(nodeBuilder(options.projectLoader, "npm install", "npm run build")));
-
-    sdm.addGoalImplementation("nodeVersioner", VersionGoal,
-        executeVersioner(options.projectLoader, NodeProjectVersioner))
-        .addGoalImplementation("nodeDockerBuild", DockerBuildGoal,
-            executeDockerBuild(
-                options.projectLoader,
-                DefaultDockerImageNameCreator,
-                NpmPreparations,
-                {
-                    ...configuration.sdm.docker as DockerOptions,
-                    dockerfileFinder: async () => "Dockerfile",
-                }))
-        .addGoalImplementation("nodeTag", TagGoal,
-            executeTag(options.projectLoader))
-        .addGoalImplementation("nodePublish", PublishGoal,
-            executePublish(options.projectLoader,
-                NodeProjectIdentifier,
-                NpmPreparations,
-                {
-                    ...configuration.sdm.npm as NpmOptions,
-                }));
-
-    sdm.goalFulfillmentMapper
-        .addSideEffect({
-            goal: StagingDeploymentGoal,
-            pushTest: IsNode,
-            sideEffectName: "@atomist/k8-automation",
-        })
-        .addSideEffect({
-            goal: ProductionDeploymentGoal,
-            pushTest: IsNode,
-            sideEffectName: "@atomist/k8-automation",
-        })
-
-        .addFullfillmentCallback({
-            goal: StagingDeploymentGoal,
-            callback: kubernetesDataCallback(options, configuration),
-        })
-        .addFullfillmentCallback({
-            goal: ProductionDeploymentGoal,
-            callback: kubernetesDataCallback(options, configuration),
-        });
+    addNodeSupport(sdm, configuration);
+    addLeinSupport(sdm, configuration);
 
     return sdm;
 }
 
-function kubernetesDataCallback(options: MachineOptions,
-                                configuration: Configuration): (goal: SdmGoal, context: RepoContext) => Promise<SdmGoal> {
-    return async (goal, ctx) => {
-        return options.projectLoader.doWithProject({
-            credentials: ctx.credentials, id: ctx.id, context: ctx.context, readOnly: true,
-        }, async p => {
-            return kubernetesDataFromGoal(goal, p, configuration);
-        });
-    };
-}
-
-function kubernetesDataFromGoal(goal: SdmGoal,
-                                p: GitProject,
-                                configuration: Configuration): Promise<SdmGoal> {
-    const ns = namespaceFromGoal(goal);
-    return createKubernetesData(
-        goal,
-        {
-            name: goal.repo.name,
-            environment: configuration.environment,
-            port: 2866,
-            ns,
-            imagePullSecret: "atomistjfrog",
-            replicas: ns === "production" ? 3 : 1,
-        },
-        p);
-}
-
-function namespaceFromGoal(goal: SdmGoal): string {
-    const name = goal.repo.name;
-    if (/-sdm$/.test(name)) {
-        return "sdm";
-    } else if (name === "k8-automation") {
-        return "k8-automation";
-    } else if (goal.environment === StagingEnvironment.replace(/\/$/, "")) {
-        return "testing";
-    } else if (goal.environment === ProductionEnvironment.replace(/\/$/, "")) {
-        return "production";
-    } else {
-        logger.debug(`Unmatched goal.environment using default namespace: ${goal.environment}`);
-        return "default";
-    }
-}
