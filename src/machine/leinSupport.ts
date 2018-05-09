0c9361eadbff09da4fdb2f3121b2d993a8d9b616
Jim Clark <slim@Jims-MacBook-Pro.local>
2018-05-09 08:49:08 -0700

    update lein support to use our cljfmt auto-fixer

--- src/machine/leinSupport.ts
+++ src/machine/leinSupport.ts
@@ -1,4 +1,4 @@
-import { Configuration } from "@atomist/automation-client";
+import { Configuration, logger, FailurePromise } from "@atomist/automation-client";
 import { GitProject } from "@atomist/automation-client/project/git/GitProject";
 import * as clj from "@atomist/clj-editors";
 import {
@@ -15,11 +15,13 @@ import {
     RunWithLogContext,
     SoftwareDeliveryMachine,
     VersionGoal,
+    editorAutofixRegistration,
 } from "@atomist/sdm";
 import * as build from "@atomist/sdm/blueprint/dsl/buildDsl";
 import { IsNode } from "@atomist/sdm/common/listener/support/pushtest/node/nodePushTests";
 import { spawnAndWatch } from "@atomist/sdm/util/misc/spawned";
 import * as df from "dateformat";
+import * as path from "path";
 
 export function addLeinSupport(sdm: SoftwareDeliveryMachine,
                                configuration: Configuration) {
@@ -42,7 +44,12 @@ export function addLeinSupport(sdm: SoftwareDeliveryMachine,
                     ...configuration.sdm.docker.jfrog as DockerOptions,
                     dockerfileFinder: async () => "docker/Dockerfile",
                 }), { pushTest: IsLein })
-
+        .addAutofixes(
+            editorAutofixRegistration(
+              {"name": "cljformat",
+               "editor": async p => {await clj.cljfmt(p.baseDir);
+                                     return p}));
+    
 }
 
 export async function MetajarPreparation(p: GitProject, rwlc: RunWithLogContext): Promise<ExecuteGoalResult> {
@@ -61,13 +68,13 @@ export async function MetajarPreparation(p: GitProject, rwlc: RunWithLogContext)
 }
 
 export const LeinProjectVersioner: ProjectVersioner = async (status, p, log) => {
-    const file = await p.findFile("./project.clj");
-    const projectVersion = clj.getVersion(file.path);
+    const file = path.join(p.baseDir,"project.clj")
+    const projectVersion = clj.getVersion(file);
     const branch = branchFromCommit(status.commit);
     const branchSuffix = branch !== status.commit.repo.defaultBranch ? `${branch}.` : "";
     const version = `${projectVersion}-${branchSuffix}${df(new Date(), "yyyymmddHHMMss")}`;
 
-    clj.setVersion(file.path, version);
+    await clj.setVersion(file,version);
 
     return version;
 };
