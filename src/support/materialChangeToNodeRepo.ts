5e86aa26b57efd088f18ec65877a786d6a6acb52
Christian Dupuis <cd@atomist.com>
2018-08-08 08:10:32 +0200

    Add some ignore files to material change check
    fixes #29
    
    [changelog:fixed]

--- src/support/materialChangeToNodeRepo.ts
+++ src/support/materialChangeToNodeRepo.ts
@@ -27,7 +27,7 @@ import {
 import * as _ from "lodash";
 
 const FilesWithExtensionToWatch = ["ts", "json", "graphql"];
-const FilesToWatch = ["Dockerfile", ".gitattributes"];
+const FilesToWatch = ["Dockerfile", ".gitattributes", ".npmignore", ".dockerignore", ".gitignore"];
 const DirectoriesToWatch = [".atomist/", "legal"];
 
 /**
